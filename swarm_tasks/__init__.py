13651ba58f11cc9d583a97274f1492653e5fcd40
NithyaSriVenkatesh <101004343+NithyaSriVenkatesh@users.noreply.github.com>
2025-03-15 14:23:34 +0530

    Update __init__.py

--- swarm_tasks/__init__.py
+++ swarm_tasks/__init__.py
@@ -1,1 +1,13 @@
+import numpy as np
+import random
 
+SEED = 42
+random.seed(SEED)
+np.random.seed(SEED)
+
+import swarm_tasks.utils
+
+#import swarm_tasks.simulation
+#import swarm_tasks.envs
+#import swarm_tasks.controllers
+#import swarm_tasks.modules
